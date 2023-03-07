aab270fde939c3cef657c315ba8ea06a26f04b81
Egor Menshikov <r3born.fs@gmail.com>
2023-03-07 15:12:06 +0300

    modified task 1_3

--- Lesson_1/1_3/Program.cs
+++ Lesson_1/1_3/Program.cs
@@ -1,6 +1,29 @@
-﻿int a = int.Parse(Console.ReadLine()!);
-int b = -a ;
-while(a >= b){
-Console.WriteLine(b);
-b ++ ;
+﻿// Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+// Решаю исходя из того что "в промежутке" означает не включительно.
+
+Console.WriteLine("enter a number: ");
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = -num1;
+
+if (num1 < num2)
+{
+    num1 += 1;
+    while (num1 < num2)
+    {
+        Console.Write(num1);
+        Console.Write(" ");
+        num1++;
+    }
+}
+else
+{
+    num2 += 1;
+    while (num2 < num1)
+    {
+        Console.Write(num2);
+        Console.Write(" ");
+        num2++;
+    }
 }
