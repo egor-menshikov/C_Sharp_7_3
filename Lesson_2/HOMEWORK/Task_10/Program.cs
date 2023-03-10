cf05d40df2aed81d860be9819a95b032ad577082
Egor Menshikov <r3born.fs@gmail.com>
2023-03-10 15:49:57 +0300

    Homework task 10 done

---
+++ Lesson_2/HOMEWORK/Task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int ThreeDigitNum = new Random().Next(100, 1000);
+Console.WriteLine($"Сгенерировано число {ThreeDigitNum}");
+
+int SecondDigit(int num)
+{
+    int result = (num / 10) % 10;
+    return result;
+}
+
+Console.WriteLine($"{SecondDigit(ThreeDigitNum)} - вторая цифра");
