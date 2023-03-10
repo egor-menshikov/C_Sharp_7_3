819a7350d0c3c8710e724ad809b2e96034ad08e0
Egor Menshikov <r3born.fs@gmail.com>
2023-03-10 15:20:25 +0300

    task 8 done

---
+++ Lesson_1/HOMEWORK/Task_8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+int num = new Random().Next(2, 1001);
+Console.WriteLine($"Сгенерировано число {num}");
+
+int count = 2;
+while (count <= num)
+{
+    Console.Write($" {count}");
+    count += 2;
+}
