db2e849254d738f1661cb46005dd7b785ae9dfb9
Egor Menshikov <r3born.fs@gmail.com>
2023-03-10 14:09:15 +0300

    task 2_1

--- Lesson_2/2_0/Program.cs
+++ Lesson_2/2_0/Program.cs
@@ -8,7 +8,7 @@ int TakeNum(int num)
 }
 
 // 123 % 10   -> 3
-// 123 // 10  -> 12
+// 123 / 10  -> 12
 int th_num = new Random().Next(100, 1000);
 int result = TakeNum(th_num);
 Console.WriteLine(result);
