5a8fb0f8a9a73e5ce7dd8ce2b7721f3aa1276564
Egor Menshikov <r3born.fs@gmail.com>
2023-03-17 14:18:41 +0300

    task 4_3

---
+++ Lesson_4/4_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// 3. Напишите программу, которая выводит массив из 8 элементов,
+//    заполненный нулями и единицами в случайном порядке.
+
+void binArr(int num)
+{
+    int[] arr = new int[num];
+    for(int i = 0; i < num; i++)
+    {
+        
+        arr[i] = new Random().Next(2);
+        Console.Write($"{arr[i]} ");
+    }
+    //Console.WriteLine("[{0}]", string.Join(", ", arr));
+}
+int len = int.Parse(Console.ReadLine()!);
+binArr(len);
