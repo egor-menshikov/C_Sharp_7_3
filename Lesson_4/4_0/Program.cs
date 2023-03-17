f09c12cb20fee58074643853783d223c6c954848
Egor Menshikov <r3born.fs@gmail.com>
2023-03-17 13:38:02 +0300

    task 4_0

--- Lesson_4/4_0/Program.cs
+++ Lesson_4/4_0/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// Напишите программу, которая принимает
+// на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+
+int sumToNum(int num)
+{
+    int sum = 0;
+    for(int i = 1; i <= num; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine("Enter a number: ");
+int a = int.Parse(Console.ReadLine()!);
+int result = sumToNum(a);
+Console.WriteLine(result);
