623a19f38b21a83c29adbdc64adf051063bc092e
Egor Menshikov <r3born.fs@gmail.com>
2023-03-18 14:07:45 +0300

    task 3 halfway done

---
+++ Lesson_4/HOMEWORK/Task_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив
+//  из N случайных элементов и выводит их на экран.
+
+
+int[] RandArr()
+{
+    Console.Write("Enter array length: ");
+    int len = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter minimal range value: ");
+    int min = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter maximum range value: ");
+    int max = int.Parse(Console.ReadLine()!);
+    
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+
+Console.WriteLine("[{0}]", string.Join(", ", RandArr()));
