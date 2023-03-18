462cda8d606f6f982822f12e1881b585ae92c4ef
Egor Menshikov <r3born.fs@gmail.com>
2023-03-18 13:02:29 +0300

    edited task 4_3

--- Lesson_4/4_3/Program.cs
+++ Lesson_4/4_3/Program.cs
@@ -6,11 +6,11 @@ void binArr(int num)
     int[] arr = new int[num];
     for(int i = 0; i < num; i++)
     {
-        
         arr[i] = new Random().Next(2);
         Console.Write($"{arr[i]} ");
     }
     //Console.WriteLine("[{0}]", string.Join(", ", arr));
 }
+Console.WriteLine("Введите длину массива: ");
 int len = int.Parse(Console.ReadLine()!);
 binArr(len);
