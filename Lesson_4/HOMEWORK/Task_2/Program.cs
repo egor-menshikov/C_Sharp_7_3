9a5ea5c49cae025d33ebdb40bcd811eb8333c198
Egor Menshikov <r3born.fs@gmail.com>
2023-03-18 13:43:30 +0300

    task 2 done

---
+++ Lesson_4/HOMEWORK/Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int numSum(int num)
+{
+    num = Math.Abs(num);
+    int result = 0;
+    
+    while (num / 10 > 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+    return result + num;
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int answer = numSum(number);
+Console.WriteLine($"Сумма цифр числа {number} равняется {answer}");
