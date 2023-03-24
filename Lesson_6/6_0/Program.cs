7868896eba2645617ceeae025cebf1a64bc11930
Egor Menshikov <r3born.fs@gmail.com>
2023-03-24 13:28:01 +0300

    6_0

--- Lesson_6/6_0/Program.cs
+++ Lesson_6/6_0/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 0. Напишите программу, которая перевернёт одномерный массив
+//    (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+// Генерация
+int[] RandArr()
+{
+    Console.Write("Enter array length: ");
+    int len = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter minimal range value: ");
+    int min = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter maximum range value: ");
+    int max = int.Parse(Console.ReadLine()!);
+    
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+
+// Вывод
+void PrintArr(int[] arr)
+{
+    Console.WriteLine(String.Empty);
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+        
+    }
+    Console.WriteLine(String.Empty);
+    Console.WriteLine(String.Empty);
+}
+
+// Работа
+void FlipArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        (arr[i], arr[arr.Length - i - 1]) = (arr[arr.Length - i - 1], arr[i]);    
+    }
+}
+
+int[] array = RandArr();
+PrintArr(array);
+FlipArr(array);
+PrintArr(array);
