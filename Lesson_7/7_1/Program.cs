44589df3686d3549ae0900f43e0eb936f124fe94
Egor Menshikov <r3born.fs@gmail.com>
2023-03-28 13:28:29 +0300

    7_1 done

--- Lesson_7/7_1/Program.cs
+++ Lesson_7/7_1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Генерируем 2d массив
+int[,] RandArr2D()
+{
+    Console.WriteLine();
+    Console.WriteLine("Generating a 2d array..");
+    Console.Write("Enter array rows number: ");
+    int line = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter array columns number: ");
+    int col = int.Parse(Console.ReadLine()!);
+
+    int[,] arr = new int[line, col];   
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            arr[i, j] = i + j;
+        }
+    }
+    return arr;
+}
+
+// Вывод 2d массива
+void PrintArr2D(int[,] arr)
+{    
+    for (int line = 0; line < arr.GetLength(0); line++)
+    {
+        for (int col = 0; col < arr.GetLength(1); col++)
+        {
+            Console.Write($"{arr[line, col]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] array = RandArr2D();
+PrintArr2D(array);
