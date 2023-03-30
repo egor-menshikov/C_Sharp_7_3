34275919c078909330889975f4d7be9f1739392d
Egor Menshikov <r3born.fs@gmail.com>
2023-03-30 11:53:07 +0300

    hw task 3 done

---
+++ Lesson_7/HOMEWORK/Task_3/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// Генерируем 2d массив
+int[,] RandArr2D()
+{
+    Console.WriteLine();
+    Console.WriteLine("Generating a 2d array..");
+    Console.Write("Enter array rows number: ");
+    int line = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter array columns number: ");
+    int col = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter minimal range value: ");
+    int min = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter maximum range value: ");
+    int max = int.Parse(Console.ReadLine()!);
+
+    int[,] arr = new int[line, col];
+    Random rand = new Random();
+
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            arr[i, j] = rand.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+// Вывод 2d массива
+void PrintArr2D(int[,] arr)
+{    
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+// Считаем среднее арифметическое столбцов (округлим до 3х знаков после запятой)
+void Average(int[,] arr)
+{
+    double sum = 0;
+    double average;
+    for (int col = 0; col < arr.GetLength(1); col++)
+    {
+        for (int line = 0; line < arr.GetLength(0); line++)
+        {
+            sum += arr[line, col];  
+        }
+    average = sum / arr.GetLength(0);
+    Console.Write($"{Math.Round(average, 3)}; ");
+    sum = 0;
+    }
+}
+
+int[,] array = RandArr2D();
+Console.WriteLine();
+PrintArr2D(array);
+Average(array);
