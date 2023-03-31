95a63b6411dcfa8fdf3d7f644d4bc09ea954cc5e
Egor Menshikov <r3born.fs@gmail.com>
2023-03-31 14:05:27 +0300

    8_1 done

--- Lesson_8/8_1/Program.cs
+++ Lesson_8/8_1/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Задайте двумерный массив. Напишите программу,
+//    которая заменяет строки на столбцы. В случае, если это невозможно,
+//    программа должна вывести сообщение для пользователя.
+
+// Генерируем 2d массив
+int[,] RandArr2D()
+{
+    Console.WriteLine();
+    Console.WriteLine("Generating a 2d array..");
+    Console.Write("Enter array rows number: ");
+    int line = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter array columns number: ");
+    int col = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter minimal range value: ");
+    int min = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter maximum range value: ");
+    int max = int.Parse(Console.ReadLine()!);
+
+    int[,] arr = new int[line, col];
+    Random rand = new Random();
+
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            arr[i, j] = rand.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+// Вывод 2d массива
+void PrintArr2D(int[,] arr)
+{
+    for (int line = 0; line < arr.GetLength(0); line++)
+    {
+        for (int col = 0; col < arr.GetLength(1); col++)
+        {
+            Console.Write($"{arr[line, col]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+// Работаем
+void TwistArr(int[,] arr)
+{
+    if (arr.GetLength(0) != arr.GetLength(1)) Console.WriteLine("We cannot work on this array.");
+    else
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                (arr[i, j], arr[j, i]) = (arr[j, i], arr[i, j]);
+            }
+        }
+    }
+}
+
+
+int[,] array = RandArr2D();
+PrintArr2D(array);
+Console.WriteLine();
+
+TwistArr(array);
+PrintArr2D(array);
