a471d703430052fb913fb8bc165bc60278ec500c
Egor Menshikov <r3born.fs@gmail.com>
2023-04-04 13:41:41 +0300

    9_0 done

---
+++ Lesson_9/9_0/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1. Задайте значение N. Напишите программу, которая выведет
+//    все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+
+void NatNum(int n)
+{
+    if (n < 1 ) return;
+    NatNum(n - 1);
+    Console.Write($"{n} ");
+}
+
+Console.WriteLine("Enter N: ");
+int num = int.Parse(Console.ReadLine()!);
+NatNum(num);
